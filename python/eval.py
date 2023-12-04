9dd7fdea9244a93bb5300b15b12580e2f2931c82
Brandon Lester <lester@amendllc.com>
2023-12-04 15:55:15 -0500

    Update eval.py

--- python/eval.py
+++ python/eval.py
@@ -26,7 +26,8 @@ data_sheetname = "Forecast Data"
 results_filename = "Doug File.xlsx"
 results_sheetname = "Export"
 
-forecast_filename = "prophet_full_forecast_4.2.20start.csv"
+forecast_filename = "official_forecast.xlsx"
+forecast_sheetname = "forecast_by_day"
 
 #%% FUNCTIONS
 def add_fake_forecasts(df_input, error_size, colname):
@@ -85,12 +86,35 @@ def create_value_bins(input_value, bin_size):
     }
 
 
-def plot_error_distribution(df, colname):
-    sns.histplot(data = df, x = colname + "_forecast_error_pct", binwidth=0.1)
+
+
+def plot_error_distribution(df, colname, barcolor):
+    x_list = list(range(-60,61,10))
+    x_ticks = [x/100 for x in x_list]
+    x_labels = [str(x)+"%" for x in x_list]
+    x_labels[0] = "<-50%"
+    x_labels[-1] = ">50%"
+    
+    sns.histplot(data = df, x = colname + "_forecast_error_pct_plot", binwidth=0.1, color = barcolor)
     plt.title(colname.title() + " Forecast Error Distribution")
     plt.xlabel("Error Percentage (%)")
     plt.ylabel("Store / Day Occurences")
-    plt.xticks(plt.xticks()[0], [int(x * 100) for x in plt.xticks()[0]])
+    #plt.xticks(plt.xticks()[0], [int(x * 100) for x in plt.xticks()[0]])
+    plt.xticks(x_ticks, x_labels)
+    
+    
+def accuracy_by_store(df, colname):
+    
+    pred_name = colname+"_forecast_orders"
+    mae_name = colname+"_mae"
+    
+    return_df = df \
+        .groupby("store_no") \
+        .agg({"actual_orders": lambda x: metrics.mean_absolute_error(y_true = x, y_pred = df_results.loc[x.index, pred_name])}) \
+        .reset_index() \
+        .rename(columns = {"actual_orders" : mae_name})
+        
+    return return_df
 
 #%% DATA PREP
 
@@ -130,7 +154,7 @@ df_results["pickup_dt"] = pd.to_datetime(df_results["pickup_dt"])
 
 
 ### AMEND FORECASTED VALUES
-forecast_import = pd.read_csv(data_path / forecast_filename)
+forecast_import = pd.read_excel(data_path / forecast_filename, sheet_name = forecast_sheetname)
 df_forecast = forecast_import.rename(columns = {"ds": "pickup_dt", "store": "store_no", "yhat": "auto_forecast_orders"})
 df_forecast = df_forecast.iloc[:,1:].astype({"store_no": "str"})
 df_forecast["pickup_dt"] = pd.to_datetime(df_forecast["pickup_dt"])
@@ -145,10 +169,24 @@ df_results = df_results.merge(df_data.drop("division_no", axis=1), on = ["pickup
 
 
 
+#%% EVALUATION
+
+### % Accuracy Metrics
+
+# add % error and absolute % error
 df_results["manual_forecast_error_pct"] = (df_results["manual_forecast_orders"] - df_results["actual_orders"]) / df_results["actual_orders"]
 df_results["auto_forecast_error_pct"] = (df_results["auto_forecast_orders"] - df_results["actual_orders"]) / df_results["actual_orders"]
 
-#%% EVALUATION
+df_results["manual_absolute_error_pct"] = abs(df_results["manual_forecast_error_pct"])
+df_results["auto_absolute_error_pct"] = abs(df_results["auto_forecast_error_pct"])
+
+# filter 0 order days (all cancelled and unable to calculate % metrics for these days)
+df_results = df_results[df_results["actual_orders"] > 0] # removes store 925 on 12/23/22 and store 344 on 12/24/22 - all orders cancelled
+
+
+df_results["manual_absolute_error_pct"].mean()
+df_results["auto_absolute_error_pct"].mean()
+
 
 # ACCURACY METRICS
 manual_metrics = calc_metrics(ytrue = df_results["actual_orders"], 
@@ -162,34 +200,30 @@ auto_metrics = calc_metrics(ytrue = df_results["actual_orders"],
 print_metrics(auto_metrics, "\nAUTO METRICS")
 
 
-# RESIDUAL DISTRIBUTION
-df_results["manual_error_bin"] = df_results["manual_forecast_error"].apply(lambda x: create_value_bins(x, 10)["bin_number"])
-sns.histplot(data = df_results, x = "manual_error_bin", binwidth=1)
 
-df_results["auto_error_bin"] = df_results["auto_forecast_error"].apply(lambda x: create_value_bins(x, 10)["bin_number"])
-sns.histplot(data = df_results, x = "auto_error_bin", binwidth=1)
+# # RESIDUAL DISTRIBUTION
+# df_results["manual_error_bin"] = df_results["manual_forecast_error"].apply(lambda x: create_value_bins(x, 10)["bin_number"])
+# sns.histplot(data = df_results, x = "manual_error_bin", binwidth=1)
+
+# df_results["auto_error_bin"] = df_results["auto_forecast_error"].apply(lambda x: create_value_bins(x, 10)["bin_number"])
+# sns.histplot(data = df_results, x = "auto_error_bin", binwidth=1)
+
+
+df_results["manual_forecast_error_pct_plot"] = np.where(df_results["manual_forecast_error_pct"] > 0.5, 0.6, df_results["manual_forecast_error_pct"])
+df_results["manual_forecast_error_pct_plot"] = np.where(df_results["manual_forecast_error_pct_plot"] < -0.5, -0.6, df_results["manual_forecast_error_pct_plot"])
+
+df_results["auto_forecast_error_pct_plot"] = np.where(df_results["auto_forecast_error_pct"] > 0.5, 0.6, df_results["auto_forecast_error_pct"])
+df_results["auto_forecast_error_pct_plot"] = np.where(df_results["auto_forecast_error_pct_plot"] < -0.5, -0.6, df_results["auto_forecast_error_pct_plot"])
+
 
 #TODO: bucket anything larger than x%
-plot_error_distribution(df_results, "manual")
-plot_error_distribution(df_results, "auto")
-
+plot_error_distribution(df_results, "manual", barcolor = "blue")
+plot_error_distribution(df_results, "auto", barcolor = "orange")
+plt.title("Auto (Orange) vs Manual (Blue) Error Distribution")
+plt.savefig('exports/error_distribution.png', dpi=300)
 
 
 # STORE SEGMENTATION
-
-def accuracy_by_store(df, colname):
-    
-    pred_name = colname+"_forecast_orders"
-    mae_name = colname+"_mae"
-    
-    return_df = df \
-        .groupby("store_no") \
-        .agg({"actual_orders": lambda x: metrics.mean_absolute_error(y_true = x, y_pred = df_results.loc[x.index, pred_name])}) \
-        .reset_index() \
-        .rename(columns = {"actual_orders" : mae_name})
-        
-    return return_df
-
 df_manual_acc = accuracy_by_store(df_results, "manual")
 df_auto_acc = accuracy_by_store(df_results, "auto")
 
@@ -206,6 +240,33 @@ df_store_agg = df_orders_agg.merge(df_manual_acc, on = "store_no").merge(df_auto
 
 sns.scatterplot(df_store_agg, x = "actual_orders_sum", y = "manual_mae")
 sns.scatterplot(df_store_agg, x = "actual_orders_sum", y = "auto_mae")
+
+
+df_store_acc_agg = df_results.groupby("store_no").agg({"manual_absolute_error_pct": "mean", 
+                                                       "auto_absolute_error_pct": "mean",
+                                                       "actual_orders": "mean"}).reset_index()
+
+sns.regplot(df_store_acc_agg, x = "actual_orders", y = "manual_absolute_error_pct")
+sns.regplot(df_store_acc_agg, x = "actual_orders", y = "auto_absolute_error_pct")
+plt.title("Auto (Orange) vs Manual (Blue) By Store Average Error")
+plt.xlabel("Daily Order Volume")
+plt.ylabel("Absolute Error Percentage")
+plt.savefig('exports/store_errors.png', dpi=300)
+
+
+
+# by week chart
+df_results["week"] = df_results['pickup_dt'].dt.strftime('%U')
+
+df_date_results = df_results.groupby("week").agg({"manual_forecast_error_pct": "mean",
+                                                       "auto_forecast_error_pct":"mean"})
+
+sns.lineplot(df_date_results, x="week", y="manual_forecast_error_pct", label="Kroger Error Percent")
+sns.lineplot(df_date_results, x="week", y="auto_forecast_error_pct", label="AMEND Error Percent").set(title='Weekly Error Percent')
+plt.xlabel('Week')
+plt.ylabel('Error Percent')
+plt.axhline(y=0, color = 'red')
+plt.legend()
 
 
 # IMPORTANT FACTORS
